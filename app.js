195347574c01d234a2bb91c4e5ecb07e965b7957
kira3691 <kiran.k@accion.labs.com>
2019-04-15 22:06:55 +0530

    Add pug templates

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'pug'); //sets the template engine to pug
+app.set('views', 'views'); // looks for template view files in view folder
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -11,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); //all static files request is directed to public folder
 
 app.use(shopRoutes)
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products)
+    res.render('shop', {prods: adminData.products, docTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
