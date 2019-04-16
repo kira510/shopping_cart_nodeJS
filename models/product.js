1d7ce3a3574ca8b39cacaca81e4dba0ac2947963
kira3691 <kiran.k@accion.labs.com>
2019-04-16 17:38:05 +0530

    Add controllers and model

---
+++ models/product.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const filePath = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(filePath, (err, fileContent)=>{
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
+module.exports = class Product {
+    constructor (title) {
+        this.title = title;
+    }
+
+    save () {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(filePath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(callBack) {
+        getProductsFromFile(callBack);
+    }
+}
