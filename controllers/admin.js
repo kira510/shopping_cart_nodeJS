9979b46975c90b8282319fa0d828c9ee1b2810c9
kira3691 <kiran.k@accion.labs.com>
2019-05-15 13:10:19 +0530

    Delete product via async req without reloading the page

--- controllers/admin.js
+++ controllers/admin.js
@@ -197,8 +197,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(prod => {
       if (!prod) {
@@ -209,11 +209,13 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({_id: prodId, userId: req.user._id});
     }).then(() => {
       console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      res.status(200).json({
+        message: 'Product Deleted!'
+      });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({
+        message: 'Deleting Product failed'
+      });
   });
 };
---
+++ public/js/admin.js
@@ -0,0 +1,25 @@
+/**
+ * This is used to delete products dynamically using asynchronous javascript
+ */
+
+const deleteProduct = (btn) => {
+    const prodId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    const parentElement = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    }).then(result => {
+        console.log(result);
+        return result.json();
+    }).then(data => {
+        console.log(data);
+        parentElement.parentNode.removeChild(parentElement);
+    }).catch(err => {
+        console.log(err);
+    });
+}
